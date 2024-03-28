d2d1eab1bea3276427ec5be47b9551c9e2587031
raka-kamara <rakakamara@outlook.com>
2024-03-28 13:43:44 +0600

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,34 +3,34 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
 
     const links = <>
-        <li className="flex">
+        <li className="flex ">
             <NavLink to="/" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>Home</NavLink>
         </li>
         <li className="flex">
             <NavLink to="/listed" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>Listed Books</NavLink>
         </li>
         <li className="flex">
             <NavLink to="/pages" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>Pages to Read</NavLink>
         </li>
         <li className="flex">
             <NavLink to="/errorPage" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>Link</NavLink>
         </li>
         <li className="flex">
             <NavLink to="/WhyReadBooks" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>WhyReadBooks?</NavLink>
         </li>
         <li className="flex">
             <NavLink to="/BuyBook" className={({isActive }) =>
-                isActive ? 'text-[#23BE0A] font-bold' : 'font-bold'
+                isActive ? 'text-[#23BE0A] font-bold border border-[#23BE0A]' : 'font-bold'
             }>Buy Book</NavLink>
         </li>
     </>;
@@ -42,11 +42,11 @@ const Header = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mx-2 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         {links}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Book Vibe</a>
+                <a className="btn btn-ghost normal-case text-xl text-[#23BE0A] font-bold">Book Vibe</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
